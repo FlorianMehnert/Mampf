1d8eea617168f7f91ce9a96da213e28cee1400a2
thejezzi <38394992+thejezzi@users.noreply.github.com>
2020-11-27 13:30:14 +0100

    updated

--- src/main/java/mampf/catalog/CatalogController.java
+++ src/main/java/mampf/catalog/CatalogController.java
@@ -1,5 +1,6 @@
 package mampf.catalog;
 
+import org.javamoney.moneta.Money;
 import org.salespointframework.inventory.UniqueInventory;
 import org.salespointframework.inventory.UniqueInventoryItem;
 import org.salespointframework.quantity.Quantity;
@@ -7,6 +8,12 @@ import org.salespointframework.time.BusinessTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import static org.salespointframework.core.Currencies.*;
+
+import mampf.catalog.Item;
+import mampf.catalog.Item.Domain;
 
 @Controller
 public class CatalogController {
@@ -27,9 +34,39 @@ public class CatalogController {
 		this.businessTime = businessTime;
 	}
 
-	@GetMapping("/items")
+	@GetMapping("/catalog")
 	String itemsCatalog(Model model){
-		return "items"; 
+		return "catalog"; 
+	}
+
+	@GetMapping("/catalog/add/random")
+	String itemsCount(Model model) {
+
+		if(catalog.count() == 0){
+			for(int i = 0; i < 20; i++){
+				catalog.save(
+				new Item(
+					"test"+ i,
+					Money.of(i, EURO),
+					Item.Domain.EVENTCATERING,
+					Item.Category.EQUIPMENT,
+					"Das ist nur ein Test"
+				)
+			);
+			}
+		}
+		model.addAttribute("count", catalog.count());
+
+		return "count";
+	}
+
+	@GetMapping("/catalog/{domain}")
+	String catalogDomain(@PathVariable String domain, Model model) {
+
+		Domain catalogDomain = Domain.valueOf(domain.toUpperCase());
+		model.addAttribute("catalog", catalog.findByDomain(catalogDomain));
+
+		return "catalog";
 	}
 	
 }
