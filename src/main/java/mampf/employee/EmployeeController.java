dd2adac84c3eea98efc68f6b7e00040b410ba446
thejezzi <38394992+thejezzi@users.noreply.github.com>
2020-11-28 20:22:25 +0100

    fixed errors

--- src/main/java/mampf/employee/EmployeeController.java
+++ src/main/java/mampf/employee/EmployeeController.java
@@ -3,6 +3,7 @@ package mampf.employee;
 import javax.validation.Valid;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
@@ -10,6 +11,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class EmployeeController {
 
 	private final EmployeeManagement employeeManagement;
