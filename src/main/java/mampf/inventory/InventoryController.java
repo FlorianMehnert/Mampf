edd569b6df4421bda54e35dcea29df9b19f106cb
Florian Mehnert <florian.mehnert@gmail.com>
2020-11-28 16:15:00 +0100

    added functionality to InventoryController

--- src/main/java/mampf/inventory/InventoryController.java
+++ src/main/java/mampf/inventory/InventoryController.java
@@ -1,13 +1,26 @@
 package mampf.inventory;
 
+import org.salespointframework.inventory.UniqueInventory;
+import org.salespointframework.inventory.UniqueInventoryItem;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class InventoryController {
+
+	private final UniqueInventory<UniqueInventoryItem> inventory;
+
+	InventoryController(UniqueInventory<UniqueInventoryItem> inventory){
+		this.inventory = inventory;
+	}
+
 	@GetMapping("/inventory")
-	public String inventory() {
+	// TODO only BOSS
+	public String inventory(Model model) {
+
+		model.addAttribute("inventory", inventory.findAll());
+
 		return "inventory";
 	}
 }
