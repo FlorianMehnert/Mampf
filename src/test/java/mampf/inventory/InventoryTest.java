8874fdb4dd54f83d7bff955be3889fc13eaedda7
Florian Mehnert <florian.mehnert@gmail.com>
2020-11-29 13:44:33 +0100

    moved inventory-funtions to InventoryController; deleted Inventory; added functionality to buttons from inventory.html

---
+++ src/test/java/mampf/inventory/InventoryTest.java
@@ -0,0 +1,36 @@
+package mampf.inventory;
+
+import mampf.catalog.MampfCatalog;
+import org.junit.jupiter.api.Test;
+import org.salespointframework.inventory.UniqueInventory;
+import org.salespointframework.inventory.UniqueInventoryItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class InventoryTest {
+	@Autowired
+	private MampfCatalog mampfCatalog;
+
+	@Autowired
+	private UniqueInventory<UniqueInventoryItem> uniqueInventory;
+
+	@Autowired
+	private InventoryController inventory;
+
+	@Test
+	public void load() throws Exception{
+		assertThat(mampfCatalog).isNotNull();
+		assertThat(uniqueInventory).isNotNull();
+		assertThat(inventory).isNotNull();
+	}
+
+	public void correctInitialization() throws Exception{
+		System.out.println(inventory.listItems());
+		assertTrue(true);
+	}
+
+}
