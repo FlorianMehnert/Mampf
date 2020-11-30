2721e9e48d7e09cff199963175f4b9231fb2634a
Florian Mehnert <florian.mehnert@gmail.com>
2020-11-30 20:12:01 +0100

    added toString Method for Category and Domain

--- src/main/java/mampf/catalog/Item.java
+++ src/main/java/mampf/catalog/Item.java
@@ -12,21 +12,81 @@ public class Item extends Product {
 	private Category category;
 
 	public static enum Domain {
-		EVENTCATERING,
-		PARTYSERVICE,
-		MOBILE_BREAKFAST,
-		RENT_A_COOK
+		EVENTCATERING{
+			@Override
+			public String toString() {
+				return "Eventcatering";
+			}
+		},
+		PARTYSERVICE{
+			@Override
+			public String toString() {
+				return "Partyservice";
+			}
+		},
+		MOBILE_BREAKFAST{
+			@Override
+			public String toString() {
+				return "Mobile Breakfast";
+			}
+		},
+		RENT_A_COOK{
+			@Override
+			public String toString() {
+				return "Rent a Cook";
+			}
+		}
 	}
 
 	public static enum Category {
-		FOOD,
-		DECORATION,
-		EQUIPMENT,
-		PERSONEL,
-		BUFFET,
-		DINNER_EVENT,
-		NONE,
-		SPECIAL_OFFERS
+		FOOD{
+			@Override
+			public String toString() {
+				return "Food";
+			}
+		},
+		DECORATION{
+			@Override
+			public String toString() {
+				return "Decoration";
+			}
+		},
+		EQUIPMENT{
+			@Override
+			public String toString() {
+				return "Equipment";
+			}
+		},
+		PERSONEL{
+			@Override
+			public String toString() {
+				return "Personel";
+			}
+		},
+		BUFFET{
+			@Override
+			public String toString() {
+				return "Buffet";
+			}
+		},
+		DINNER_EVENT{
+			@Override
+			public String toString() {
+				return "Dinner Event";
+			}
+		},
+		NONE{
+			@Override
+			public String toString() {
+				return "";
+			}
+		},
+		SPECIAL_OFFERS{
+			@Override
+			public String toString() {
+				return "Special Offers";
+			}
+		}
 	}
 
 	@SuppressWarnings({"unused", "deprecation"})
