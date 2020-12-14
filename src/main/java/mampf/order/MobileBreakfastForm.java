42a55ba01983f147617ba627695eac2bf7d06b31
Florian Mehnert <florian.mehnert@gmail.com>
2020-12-14 21:06:30 +0100

    fixed #12

--- src/main/java/mampf/order/MobileBreakfastForm.java
+++ src/main/java/mampf/order/MobileBreakfastForm.java
@@ -4,74 +4,53 @@ import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.annotation.Nullable;
-import javax.validation.constraints.NotNull;
 
 import mampf.catalog.BreakfastItem;
 
 public class MobileBreakfastForm {
-	@NotNull(message = "beverage should be set")
 	private final BreakfastItem beverage;
 
-	@NotNull(message = "dish should be set")
 	private final BreakfastItem dish;
 
-	@Nullable
 	private final boolean monday;
-	@Nullable
 	private final boolean tuesday;
-	@Nullable
 	private final boolean wednesday;
-	@Nullable
 	private final boolean thursday;
-	@Nullable
 	private final boolean friday;
 
 	private final String time;
 
 	public MobileBreakfastForm(
-		BreakfastItem beverage, 
-		BreakfastItem dish,
-		String monday, 
-		String tuesday, 
-		String wednesday,
-		String thursday, 
-		String friday,
-		String time
-		) {
+			BreakfastItem beverage,
+			BreakfastItem dish,
+			String monday,
+			String tuesday,
+			String wednesday,
+			String thursday,
+			String friday,
+			String time
+	) {
 		this.beverage = beverage;
 		this.dish = dish;
-		if(monday != null)
-			this.monday = true;
-		else
-			this.monday = false;
-		if(tuesday != null)
-			this.tuesday = true;
-		else
-			this.tuesday = false;
-		if(wednesday != null)
-			this.wednesday = true;
-		else
-			this.wednesday = false;
-		if(thursday != null)
-			this.thursday = true;
-		else
-			this.thursday = false;
-		if(friday != null)
-			this.friday = true;
-		else
-			this.friday = false;
+		this.monday = monday != null;
+		this.tuesday = tuesday != null;
+		this.wednesday = wednesday != null;
+		this.thursday = thursday != null;
+		this.friday = friday != null;
 
 		// hh:mm
 		this.time = time;
 	}
-	public BreakfastItem getBeverage(){
+
+	public BreakfastItem getBeverage() {
 		return beverage;
 	}
-	public BreakfastItem getDish(){
+
+	public BreakfastItem getDish() {
 		return dish;
 	}
-	public Map<String, Boolean> getDays(){
+
+	public Map<String, Boolean> getDays() {
 		Map<String, Boolean> days = new HashMap<>();
 		days.put("monday", this.monday);
 		days.put("tuesday", this.tuesday);
@@ -80,7 +59,8 @@ public class MobileBreakfastForm {
 		days.put("friday", this.friday);
 		return days;
 	}
-	public LocalTime getTime(){
+
+	public LocalTime getTime() {
 		return LocalTime.parse(this.time);
 	}
 
